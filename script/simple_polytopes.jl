2c09d084fec702c200cf580fb08dd9dce9f19d55
Brian Doolittle <brian.d.doolittle@gmail.com>
2021-03-05 16:46:59 -0600

    scratch work for multi-access channel bell inequalities and violations

---
+++ script/simple_polytopes.jl
@@ -0,0 +1,387 @@
+using Test
+using BellScenario
+
+include("../src/MultiAccessChannels.jl")
+
+@testset "simple polytopes" begin
+    @testset "(2,2) -> (2,2) -> 2 only has non-negativity facets" begin
+        (X, Y, Z, dA, dB) = (2, 2, 2, 2, 2)
+
+        vertices = multi_access_vertices(X,Y,Z,dA,dB)
+
+        facet_dict = LocalPolytope.facets(vertices)
+
+        facets = facet_dict["facets"]
+
+        @test length(facets) == 8
+        @test facet_dict["equalities"] == []
+
+        bell_games = map(f -> convert(BellGame, f, BlackBox(2,4),rep="normalized"), facets)
+
+        @test bell_games == [
+            [0 0 0 0;1 0 0 0],[0 0 0 0;0 1 0 0],[0 0 0 0;0 0 1 0],[0 0 0 0;0 0 0 1],
+            [0 0 0 1;0 0 0 0],[0 0 1 0;0 0 0 0],[0 1 0 0;0 0 0 0],[1 0 0 0;0 0 0 0]
+        ]
+    end
+
+    @testset "(2,2) -> (2,2) -> 3 only has non-negativity facets" begin
+        (X, Y, Z, dA, dB) = (2, 2, 3, 2, 2)
+
+        vertices = multi_access_vertices(X,Y,Z,dA,dB)
+
+        facet_dict = LocalPolytope.facets(vertices)
+
+        facets = facet_dict["facets"]
+
+        @test length(facets) == 12
+        @test facet_dict["equalities"] == []
+
+        bell_games = map(f -> convert(BellGame, f, BlackBox(3,4),rep="normalized"), facets)
+
+        @test bell_games == [
+            [0 0 0 0; 1 0 0 0; 1 0 0 0],[1 0 0 0; 0 0 0 0; 1 0 0 0],[0 0 0 0; 0 1 0 0; 0 1 0 0],
+            [0 1 0 0; 0 0 0 0; 0 1 0 0],[0 0 0 0; 0 0 1 0; 0 0 1 0],[0 0 1 0; 0 0 0 0; 0 0 1 0],
+            [0 0 0 0; 0 0 0 1; 0 0 0 1],[0 0 0 1; 0 0 0 0; 0 0 0 1],[0 0 0 1; 0 0 0 1; 0 0 0 0],
+            [0 0 1 0; 0 0 1 0; 0 0 0 0],[0 1 0 0; 0 1 0 0; 0 0 0 0],[1 0 0 0; 1 0 0 0; 0 0 0 0]
+        ]
+    end
+
+    @testset "(2,2) -> (2,2) -> 4 only has non-negativity facets" begin
+        (X, Y, Z, dA, dB) = (2, 2, 4, 2, 2)
+
+        vertices = multi_access_vertices(X,Y,Z,dA,dB)
+
+        facet_dict = LocalPolytope.facets(vertices)
+
+        facets = facet_dict["facets"]
+
+        @test length(facets) == 16
+        @test facet_dict["equalities"] == []
+
+        bell_games = map(f -> convert(BellGame, f, BlackBox(4,4),rep="normalized"), facets)
+
+        @test bell_games == [
+            [0 0 0 0; 1 0 0 0; 1 0 0 0; 1 0 0 0],[1 0 0 0; 0 0 0 0; 1 0 0 0; 1 0 0 0],
+            [1 0 0 0; 1 0 0 0; 0 0 0 0; 1 0 0 0],[0 0 0 0; 0 1 0 0; 0 1 0 0; 0 1 0 0],
+            [0 1 0 0; 0 0 0 0; 0 1 0 0; 0 1 0 0],[0 1 0 0; 0 1 0 0; 0 0 0 0; 0 1 0 0],
+            [0 0 0 0; 0 0 1 0; 0 0 1 0; 0 0 1 0],[0 0 1 0; 0 0 0 0; 0 0 1 0; 0 0 1 0],
+            [0 0 1 0; 0 0 1 0; 0 0 0 0; 0 0 1 0],[0 0 0 0; 0 0 0 1; 0 0 0 1; 0 0 0 1],
+            [0 0 0 1; 0 0 0 0; 0 0 0 1; 0 0 0 1],[0 0 0 1; 0 0 0 1; 0 0 0 0; 0 0 0 1],
+            [0 0 0 1; 0 0 0 1; 0 0 0 1; 0 0 0 0],[0 0 1 0; 0 0 1 0; 0 0 1 0; 0 0 0 0],
+            [0 1 0 0; 0 1 0 0; 0 1 0 0; 0 0 0 0],[1 0 0 0; 1 0 0 0; 1 0 0 0; 0 0 0 0],
+        ]
+    end
+
+    @testset "(2,2) -> (2,2) -> 5 only has non-negativity facets" begin
+        (X, Y, Z, dA, dB) = (2, 2, 5, 2, 2)
+
+        vertices = multi_access_vertices(X,Y,Z,dA,dB)
+
+        facet_dict = LocalPolytope.facets(vertices)
+
+        facets = facet_dict["facets"]
+
+        @test length(facets) == 20
+        @test facet_dict["equalities"] == []
+
+        bell_games = map(f -> convert(BellGame, f, BlackBox(5,4),rep="normalized"), facets)
+
+        @test bell_games == [
+            [0 0 0 0; 1 0 0 0; 1 0 0 0; 1 0 0 0; 1 0 0 0], [1 0 0 0; 0 0 0 0; 1 0 0 0; 1 0 0 0; 1 0 0 0],
+            [1 0 0 0; 1 0 0 0; 0 0 0 0; 1 0 0 0; 1 0 0 0], [1 0 0 0; 1 0 0 0; 1 0 0 0; 0 0 0 0; 1 0 0 0],
+            [0 0 0 0; 0 1 0 0; 0 1 0 0; 0 1 0 0; 0 1 0 0], [0 1 0 0; 0 0 0 0; 0 1 0 0; 0 1 0 0; 0 1 0 0],
+            [0 1 0 0; 0 1 0 0; 0 0 0 0; 0 1 0 0; 0 1 0 0], [0 1 0 0; 0 1 0 0; 0 1 0 0; 0 0 0 0; 0 1 0 0],
+            [0 0 0 0; 0 0 1 0; 0 0 1 0; 0 0 1 0; 0 0 1 0], [0 0 1 0; 0 0 0 0; 0 0 1 0; 0 0 1 0; 0 0 1 0],
+            [0 0 1 0; 0 0 1 0; 0 0 0 0; 0 0 1 0; 0 0 1 0], [0 0 1 0; 0 0 1 0; 0 0 1 0; 0 0 0 0; 0 0 1 0],
+            [0 0 0 0; 0 0 0 1; 0 0 0 1; 0 0 0 1; 0 0 0 1], [0 0 0 1; 0 0 0 0; 0 0 0 1; 0 0 0 1; 0 0 0 1],
+            [0 0 0 1; 0 0 0 1; 0 0 0 0; 0 0 0 1; 0 0 0 1], [0 0 0 1; 0 0 0 1; 0 0 0 1; 0 0 0 0; 0 0 0 1],
+            [0 0 0 1; 0 0 0 1; 0 0 0 1; 0 0 0 1; 0 0 0 0], [0 0 1 0; 0 0 1 0; 0 0 1 0; 0 0 1 0; 0 0 0 0],
+            [0 1 0 0; 0 1 0 0; 0 1 0 0; 0 1 0 0; 0 0 0 0], [1 0 0 0; 1 0 0 0; 1 0 0 0; 1 0 0 0; 0 0 0 0]
+        ]
+    end
+
+    @testset "(3,2) -> (2,2) -> 2" begin
+        (X, Y, Z, dA, dB) = (3, 2, 2, 2, 2)
+
+        vertices = multi_access_vertices(X,Y,Z,dA,dB)
+        @test length(vertices) == 40
+        @test length(vertices) == multi_access_num_vertices(X,Y,Z,dA,dB)
+
+        facet_dict = LocalPolytope.facets(vertices)
+
+        facets = facet_dict["facets"]
+
+        @test length(facets) == 36
+        @test facet_dict["equalities"] == []
+
+        bell_games = map(f -> convert(BellGame, f, BlackBox(2,6),rep="normalized"), facets)
+
+        classes_dict = facet_classes(X, Y, Z, bell_games)
+
+        @test length(keys(classes_dict)) == 2
+
+        bg1 = bell_games[findfirst(bg -> bg in classes_dict[1], bell_games)]
+        @test bg1 == [
+            0  0  0  0  0  0;
+            1  0  0  0  0  0;
+        ]
+        @test bg1.β == 1
+
+        bg2 = bell_games[findfirst(bg -> bg in classes_dict[2], bell_games)]
+        @test bg2 == [
+            0  0  0  1  1  0;
+            1  1  1  0  0  0;
+        ]
+        @test bg2.β == 4
+    end
+
+    @testset "(2,3) -> (2,2) -> 2" begin
+        (X, Y, Z, dA, dB) = (2, 3, 2, 2, 2)
+
+        vertices = multi_access_vertices(X,Y,Z,dA,dB)
+        @test length(vertices) == 40
+        @test length(vertices) == multi_access_num_vertices(X,Y,Z,dA,dB)
+
+        facet_dict = LocalPolytope.facets(vertices)
+
+        facets = facet_dict["facets"]
+
+        @test length(facets) == 36
+        @test facet_dict["equalities"] == []
+
+        bell_games = map(f -> convert(BellGame, f, BlackBox(2,6),rep="normalized"), facets)
+
+        classes_dict = facet_classes(X, Y, Z, bell_games)
+
+        @test length(keys(classes_dict)) == 2
+
+        bg1 = bell_games[findfirst(bg -> bg in classes_dict[1], bell_games)]
+        @test bg1 == [
+            0  0  0  0  0  0;
+            1  0  0  0  0  0;
+        ]
+        @test bg1.β == 1
+
+        bg2 = bell_games[findfirst(bg -> bg in classes_dict[2], bell_games)]
+        @test bg2 == [
+            0  0  1  0  1  0;
+            1  1  0  1  0  0;
+        ]
+        @test bg2.β == 4
+    end
+
+    @testset "(4,2) -> (2,2) -> 2" begin
+        (X, Y, Z, dA, dB) = (4, 2, 2, 2, 2)
+
+        vertices = multi_access_vertices(X,Y,Z,dA,dB)
+        @test length(vertices) == 88
+        @test length(vertices) == multi_access_num_vertices(X,Y,Z,dA,dB)
+
+        facet_dict = LocalPolytope.facets(vertices)
+
+        facets = facet_dict["facets"]
+
+        @test length(facets) == 136
+        @test facet_dict["equalities"] == []
+
+        bell_games = map(f -> convert(BellGame, f, BlackBox(2,8),rep="normalized"), facets)
+
+        classes_dict = facet_classes(X, Y, Z, bell_games)
+
+        @test length(keys(classes_dict)) == 3
+
+        bg1 = bell_games[findfirst(bg -> bg in classes_dict[1], bell_games)]
+        @test bg1 == [
+            0  0  0  0  0  0  0  0;
+            1  0  0  0  0  0  0  0;
+        ]
+        @test bg1.β == 1
+
+        bg2 = bell_games[findfirst(bg -> bg in classes_dict[2], bell_games)]
+        @test bg2 == [
+            0  0  0  1  0  0  1  0;
+            1  1  1  0  0  0  0  0;
+        ]
+        @test bg2.β == 4
+
+        bg3 = bell_games[findfirst(bg -> bg in classes_dict[3], bell_games)]
+        @test bg3 == [
+            0  0  0  1  1  0  1  1;
+            1  1  1  0  0  1  0  0;
+        ]
+        @test bg3.β == 6
+    end
+
+    @testset "(3,3) -> (2,2) -> 2" begin
+        (X, Y, Z, dA, dB) = (3, 3, 2, 2, 2)
+
+        vertices = multi_access_vertices(X,Y,Z,dA,dB)
+
+        @test multi_access_num_vertices(X,Y,Z,dA,dB) == length(vertices)
+        @test length(vertices) == 104
+
+        facet_dict = LocalPolytope.facets(vertices)
+
+        facets = facet_dict["facets"]
+
+        @test length(facets) == 1230
+        @test facet_dict["equalities"] == []
+
+        bell_games = map(f -> convert(BellGame, f, BlackBox(2,9),rep="normalized"), facets)
+
+        classes_dict = facet_classes(X, Y, Z, bell_games)
+
+        @test length(keys(classes_dict)) == 20
+
+        bg1 = bell_games[findfirst(bg -> bg in classes_dict[1], bell_games)]
+        @test bg1 == [
+            0  0  0  0  0  0  0  0  0;
+            1  0  0  0  0  0  0  0  0;
+        ]
+        @test bg1.β == 1
+
+        bg2 = bell_games[findfirst(bg -> bg in classes_dict[2], bell_games)]
+        @test bg2 == [
+            0  0  0  0  1  0  1  0  0;
+            1  1  0  1  0  0  0  0  0;
+        ]
+        @test bg2.β == 4
+
+        bg3 = bell_games[findfirst(bg -> bg in classes_dict[3], bell_games)]
+        @test bg3 == [
+            0  0  1  0  1  0  0  0  0;
+            1  1  0  1  0  0  0  0  0;
+        ]
+        @test bg3.β == 4
+
+        bg4 = bell_games[findfirst(bg -> bg in classes_dict[4], bell_games)]
+        @test bg4.β == 5
+        @test bg4 == [
+            0  0  0  0  0  1  1  0  0;
+            1  1  0  1  0  0  0  1  0;
+        ]
+
+        bg5 = bell_games[findfirst(bg -> bg in classes_dict[5], bell_games)]
+        @test bg5 == [
+            0  0  0  0  1  0  0  0  1;
+            1  1  0  1  0  1  0  0  0;
+        ]
+        @test bg5.β == 5
+
+        bg6 = bell_games[findfirst(bg -> bg in classes_dict[6], bell_games)]
+        @test bg6.β == 7
+        @test bg6 == [
+            0  0  1  0  1  0  1  0  0;
+            2  1  0  1  0  1  0  1  0;
+        ]
+
+        bg7 = bell_games[findfirst(bg -> bg in classes_dict[7], bell_games)]
+        @test bg7.β == 7
+        @test bg7 == [
+            0  0  1  0  1  0  1  0  0;
+            2  2  0  1  0  0  0  1  0;
+        ]
+
+        bg8 = bell_games[findfirst(bg -> bg in classes_dict[8], bell_games)]
+        @test bg8.β == 7
+        @test bg8 == [
+            0  0  1  0  1  0  1  0  0;
+            2  1  0  2  0  1  0  0  0;
+        ]
+
+        bg9 = bell_games[findfirst(bg -> bg in classes_dict[9], bell_games)]
+        @test bg9.β == 6
+        @test bg9 == [
+            0  0  0  0  1  1  1  0  1;
+            1  1  0  1  0  0  0  1  0;
+        ]
+
+        bg10 = bell_games[findfirst(bg -> bg in classes_dict[10], bell_games)]
+        @test bg10.β == 8
+        @test bg10 ==  [
+            0  0  0  0  1  1  2  0  0;
+            2  2  0  1  0  0  0  1  0;
+        ]
+
+        bg11 = bell_games[findfirst(bg -> bg in classes_dict[11], bell_games)]
+        @test bg11.β == 8
+        @test bg11 == [
+            0  0  0  0  2  0  1  0  1;
+            2  2  0  1  0  1  0  0  0;
+        ]
+
+        bg12 = bell_games[findfirst(bg -> bg in classes_dict[12], bell_games)]
+        @test bg12 == [
+            0  0  1  0  2  0  0  0  1
+            2  1  0  2  0  1  0  0  0
+        ]
+        @test bg12.β == 8
+
+        bg13 = bell_games[findfirst(bg -> bg in classes_dict[13], bell_games)]
+        @test bg13 == [
+            0  0  1  0  2  0  0  0  1;
+            2  1  0  2  0  0  0  1  0;
+        ]
+        @test bg13.β == 8
+
+        bg14 = bell_games[findfirst(bg -> bg in classes_dict[14], bell_games)]
+        @test bg14 == [
+            0  0  1  0  1  0  2  0  0;
+            2  1  0  0  0  1  0  2  0;
+        ]
+        @test bg14.β == 8
+
+        bg15 = bell_games[findfirst(bg -> bg in classes_dict[15], bell_games)]
+        @test bg15 == [
+            0  0  1  0  2  0  1  0  1;
+            3  2  0  2  0  1  0  0  0;
+        ]
+        @test bg15.β == 10
+
+        bg16 = bell_games[findfirst(bg -> bg in classes_dict[16], bell_games)]
+        @test  bg16 == [
+            0  0  1  0  2  0  1  0  1;
+            3  2  0  2  0  0  0  1  0;
+        ]
+        @test bg16.β == 10
+
+        bg17 = bell_games[findfirst(bg -> bg in classes_dict[17], bell_games)]
+        @test bg17.β == 11
+        @test bg17 == [
+            0  0  2  0  1  0  2  0  0;
+            3  1  0  1  0  2  0  2  0;
+        ]
+
+        bg18 = bell_games[findfirst(bg -> bg in classes_dict[18], bell_games)]
+        @test bg18 == [
+            0  0  2  0  2  0  2  0  0;
+            3  1  0  1  0  3  0  3  1;
+        ]
+        @test bg18.β == 14
+
+        bg19 = bell_games[findfirst(bg -> bg in classes_dict[19], bell_games)]
+        @test bg19 == [
+            0  0  2  0  3  0  2  0  1;
+            5  3  0  3  0  1  0  1  0;
+        ]
+        @test bg19.β == 16
+
+        bg20 = bell_games[findfirst(bg -> bg in classes_dict[20], bell_games)]
+        @test bg20 == [
+            0  0  2  1  2  0  5  0  1;
+            4  2  0  0  0  1  0  4  0;
+        ]
+        @test bg20.β == 17
+    end
+
+    @testset "(3,3) -> (2,2) -> 3" begin
+        (X, Y, Z, dA, dB) = (3, 3, 3, 2, 2)
+
+        vertices = multi_access_vertices(X,Y,Z,dA,dB)
+
+        @test length(vertices) == 633
+        @test length(vertices) == multi_access_num_vertices(X,Y,Z,dA,dB)
+
+        # facet computation is too expensive
+    end
+end
---
+++ src/MultiAccessChannels.jl
@@ -0,0 +1,298 @@
+using LinearAlgebra
+using Combinatorics
+using QBase
+using BellScenario
+using Base.Iterators: flatten
+
+using Convex
+using SCS
+
+"""
+    multi_access_vertices()
+
+Computes the bipartite multi-access channel vertices using brute force and taking
+the unique combinations.
+"""
+function multi_access_vertices(X :: Int64, Y :: Int64, Z :: Int64, dA :: Int64, dB :: Int64) :: Vector{Vector{Int64}}
+
+    P_A = BlackBox(dA,X)
+    P_B = BlackBox(dB,Y)
+    P_C = BlackBox(Z,dA*dB)
+
+    P_A_vertices = deterministic_strategies(P_A)
+    P_B_vertices = deterministic_strategies(P_B)
+    P_C_vertices = deterministic_strategies(P_C)
+
+    num_verts_raw = dA^X*dB^Y*Z^(dA*dB)
+    verts = Vector{Vector{Int64}}(undef, num_verts_raw)
+
+    id = 1
+    for v_A in P_A_vertices, v_B in P_B_vertices, v_C in P_C_vertices
+        V = v_C * kron(v_A,v_B)
+        verts[id] = V[1:end-1,:][:]
+        id += 1
+    end
+
+    unique(verts)
+end
+
+"""
+    multi_access_num_bit_vertices(X :: Int64, Y :: Int64, Z :: Int64) :: Int64
+
+Counts the number of multi-access channel vertices when no more than a bit is used
+per signaling party.
+"""
+function multi_access_num_bit_vertices(X :: Int64, Y :: Int64, Z :: Int64) :: Int64
+    dA = 2
+    dB = 2
+
+    # dA = dB = 1 case
+    num_vs = Z
+
+    # dA = 2, dB = 1 case or dA = 1, dB = 2
+    num_vs += sum(cA -> LocalPolytope.num_vertices(LocalSignaling(X,Z,cA), rank_d_only=true), 2:dA)
+    num_vs += sum(cB -> LocalPolytope.num_vertices(LocalSignaling(Y,Z,cB), rank_d_only=true), 2:dB)
+
+    # dA = dB = 2 case
+    dC = dA*dB
+    for c in 2:min(Z,dC)
+        num_non_unique = 0
+        if c ≤ dA
+            num_non_unique += QMath.stirling2(dA,c)
+        end
+        if c ≤ dB
+            num_non_unique += QMath.stirling2(dB,c)
+        end
+
+        num_vs += QMath.stirling2(X,dA)*QMath.stirling2(Y,dB)*(QMath.stirling2(dC,c)-num_non_unique) * binomial(Z,c)*factorial(c)
+    end
+
+    num_vs
+end
+
+"""
+    multi_access_num_vertices(X :: Int64, Y :: Int64, Z :: Int64, dA :: Int64, dB :: Int64) :: Int64
+
+Counts the number of multi-access channel vertices. This method is currently wrong for cases where dA or
+dB are not equal to 2.
+"""
+function multi_access_num_vertices(X :: Int64, Y :: Int64, Z :: Int64, dA :: Int64, dB :: Int64) :: Int64
+    println("Warning this method is not working properly for dA >  2 or  dB > 2")
+
+    num_vs = Z
+
+    num_vs += sum(cA -> LocalPolytope.num_vertices(LocalSignaling(X,Z,cA), rank_d_only=true), 2:dA)
+    num_vs += sum(cB -> LocalPolytope.num_vertices(LocalSignaling(Y,Z,cB), rank_d_only=true), 2:dB)
+
+    dC = dA*dB
+    for c in 2:min(Z,dC)
+        num_non_unique = 0
+        if c ≤ dA || c ≤ dB
+            if c ≤ dA
+                for cA in c:dA
+                    num_non_unique += QMath.stirling2(dA,cA)
+                end
+            end
+            if c ≤ dB
+                for cB in c:dB
+                    num_non_unique += QMath.stirling2(dB,cB)
+                end
+            end
+
+        elseif c ≤ 4
+            # for cC in c:4
+            #     num_non_unique += QMath.stirling2(dC, cC)
+            # end
+        end
+
+        num_vs += QMath.stirling2(X,dA)*QMath.stirling2(Y,dB)*(QMath.stirling2(dC,c)-num_non_unique) * binomial(Z,c)*factorial(c)
+    end
+
+    num_vs
+end
+
+"""
+function to perform permutations on a tensor product structure
+
+
+"""
+function multi_access_input_permutations(X :: Int64, Y :: Int64)
+
+    prod_perms = Vector{Vector{Int64}}(undef, factorial(X)*factorial(Y))
+    prod_perm_id = 1
+
+    for x_perm in permutations(1:X), y_perm in permutations(1:Y)
+        prod_perm = zeros(Int64, X*Y)
+
+        for x in 1:X, y in 1:Y
+            # find id in product
+            prod_id = n_product_id([x,y],[X,Y])
+            perm_prod_id = n_product_id([x_perm[x],y_perm[y]],[X,Y])
+
+            # set as id of contents
+            prod_perm[prod_id] = perm_prod_id
+        end
+
+        prod_perms[prod_perm_id] = prod_perm
+        prod_perm_id += 1
+    end
+
+    prod_perms
+end
+
+function multi_access_output_permutations(Z :: Int64)
+    permutations(1:Z)
+end
+
+function tripartite_input_permutations(X :: Int64, Y :: Int64, Z :: Int64)
+
+    prod_perms = Vector{Vector{Int64}}(undef, factorial(X)*factorial(Y)*factorial(Z))
+    prod_perm_id = 1
+
+    for x_perm in permutations(1:X), y_perm in permutations(1:Y), z_perm in permutations(1:Z)
+        prod_perm = zeros(Int64, X*Y*Y)
+
+        for x in 1:X, y in 1:Y, z in 1:Z
+            # find id in product
+            prod_id = n_product_id([x,y,z],[X,Y,Z])
+            perm_prod_id = n_product_id([x_perm[x],y_perm[y],z_perm[z]],[X,Y,Z])
+
+            # set as id of contents
+            prod_perm[prod_id] = perm_prod_id
+        end
+
+        prod_perms[prod_perm_id] = prod_perm
+        prod_perm_id += 1
+    end
+
+    prod_perms
+end
+
+function quadpartite_input_permutations(W :: Int64, X :: Int64, Y :: Int64, Z :: Int64)
+
+    prod_perms = Vector{Vector{Int64}}(undef, factorial(W)*factorial(X)*factorial(Y)*factorial(Z))
+    prod_perm_id = 1
+
+    for w_perm in permutations(1:W), x_perm in permutations(1:X), y_perm in permutations(1:Y), z_perm in permutations(1:Z)
+        prod_perm = zeros(Int64, W*X*Y*Y)
+
+        for w in 1:W, x in 1:X, y in 1:Y, z in 1:Z
+            # find id in product
+            prod_id = n_product_id([w,x,y,z],[W,X,Y,Z])
+            perm_prod_id = n_product_id([w_perm[w],x_perm[x],y_perm[y],z_perm[z]],[W,X,Y,Z])
+
+            # set as id of contents
+            prod_perm[prod_id] = perm_prod_id
+        end
+
+        prod_perms[prod_perm_id] = prod_perm
+        prod_perm_id += 1
+    end
+
+    prod_perms
+end
+
+
+function n_product_id(party_ids :: Vector{Int64}, party_inputs::Vector{Int64})
+    num_ids = length(party_ids)
+    if !all(x -> party_inputs[x] ≥ party_ids[x] ≥ 1, 1:num_ids)
+        throw(DomainError(party_inputs, "not all num inputs are valid"))
+    elseif num_ids != length(party_inputs)
+        throw(DomainError(party_ids, "id does not contain the right number of elements"))
+    end
+
+    id = 1
+    multiplier = 1
+    for i in num_ids:-1:1
+        multiplier *= (i+1 > num_ids) ? 1 : party_inputs[i+1]
+        id += (party_ids[i] - 1) * multiplier
+    end
+
+    id
+end
+
+function facet_classes(X, Y, Z, bell_games :: Vector{BellScenario.BellGame})
+    input_perms = multi_access_input_permutations(X,Y)
+    output_perms = multi_access_output_permutations(Z)
+    num_perms = length(input_perms)*length(output_perms)
+
+    facet_class_dict = Dict{Int64, Vector{Matrix{Int64}}}()
+    facet_class_id = 1
+
+    for bell_game in bell_games
+        facet_considered = false
+
+        for facet_class in values(facet_class_dict)
+            if bell_game in facet_class
+                facet_considered = true
+                break
+            end
+        end
+
+        if !facet_considered
+            # construct all unique permutations
+            perms = Array{Matrix{Int64}}(undef, num_perms)
+            id = 1
+            for input_perm in input_perms, output_perm in output_perms
+                perms[id] = bell_game[output_perm, input_perm]
+                id += 1
+            end
+
+            # add facet to facet dictionary
+            facet_class_dict[facet_class_id] = unique(perms)
+            facet_class_id += 1
+        end
+    end
+
+    facet_class_dict
+end
+
+function multi_access_optimize_measurement(
+    X,Y,Z,dA,dB,
+    game::BellGame,
+    ρA_states::Vector{<:States.AbstractDensityMatrix},
+    ρB_states::Vector{<:States.AbstractDensityMatrix},
+) :: Dict
+    # if scenario.X != length(ρ_states)
+    #     throw(DomainError(scenario, "expected length of `ρ_states` is $(scenario.X)), but got $(length(ρ_states)) instead"))
+    # end
+    #
+    # if size(ρ_states[1]) != (scenario.d,scenario.d)
+    #     throw(DomainError(ρ_states, "dimension of `ρ_states` is not $(scenario.d)"))
+    # end
+
+    ρ_states = collect(flatten( map( ρA -> map(ρB -> kron(ρA,ρB), ρB_states), ρA_states)))
+
+
+    norm_game_vector = convert(Vector{Int64}, game)
+    norm_bound = norm_game_vector[end]
+    norm_game = reshape(norm_game_vector[1:(end-1)], (Z-1, X*Y))
+
+    # add povm variables and constraints
+    Π_vars = map(i -> HermitianSemidefinite(dA*dB), 1:Z)
+    constraints = (sum(map(Π_y -> real(Π_y), Π_vars)) == Matrix{Float64}(I, dA*dB, dA*dB))
+    constraints += (sum(map(Π_y -> imag(Π_y), Π_vars)) == zeros(Float64, dA*dB, dA*dB))
+
+    # sum up the state contibutions for each row
+    H_y = map(row_id -> sum(norm_game[row_id,:] .* ρ_states), 1:Z-1)
+
+    # add the objective
+    objective = maximize(real(tr(sum(Π_vars[1:end-1] .* H_y))), constraints)
+
+    # optimize model
+    solve!(objective, SCS.Optimizer(verbose=0))
+
+    # parse/return results
+    score = objective.optval
+    violation = score - norm_bound
+    # Π_opt = _opt_vars_to_povm(map(Π_y -> Π_y.value, Π_vars))
+    Π_opt = map(Π_y -> Π_y.value, Π_vars)
+
+    Dict(
+        "violation" => violation,
+        "povm" => Π_opt,
+        "game" => game,
+        "scenario" => (X,Y,Z,dA,dB),
+        "states" => ρ_states
+    )
+end
