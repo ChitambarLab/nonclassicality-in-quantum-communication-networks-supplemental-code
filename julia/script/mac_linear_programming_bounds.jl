db87a5296e659e5f86305f8717744a215ea4309b
Brian Doolittle <brian.d.doolittle@gmail.com>
2023-11-29 13:54:15 -0500

    cleaning up multiaccess network script

--- julia/script/mac_linear_programming_bounds.jl
+++ julia/script/mac_linear_programming_bounds.jl
@@ -60,7 +60,7 @@ end
     raw_game = optimize_linear_witness(vertices, diff_test[1:2,:][:])
     bell_game = convert(BellGame, round.(Int, 2*raw_game), BlackBox(3,9), rep="normalized")
 
-    @testt bell_game.β == 8
+    @test bell_game.β == 8
     @test bell_game == [
         2  0  0  0  1  0  0  0  1;
         0  0  1  2  0  1  0  1  0;
@@ -136,78 +136,35 @@ end
     @test 8 == max(map(v -> sum(v[:].*qkd_game[:]), vertices44_22_4)...)
 end
 
- @testset "44->22->4 MAC Game" begin
+# NOTE: Test takes a long time to run
+# @testset "44->22->4 MAC Game" begin
         
-    vertices_44_22_4 = multi_access_vertices(4,4,4,2,2, normalize=false)
+#     vertices_44_22_4 = multi_access_vertices(4,4,4,2,2, normalize=false)
 
-    vertices_44_34_4 = multi_access_vertices(4,4,4,3,4, normalize=false)
-    vertices_44_43_4 = multi_access_vertices(4,4,4,4,3, normalize=false)
+#     vertices_44_34_4 = multi_access_vertices(4,4,4,3,4, normalize=false)
+#     vertices_44_43_4 = multi_access_vertices(4,4,4,4,3, normalize=false)
 
-    vertices_44_3443_4 = unique(cat(
-        vertices_44_34_4,
-        vertices_44_43_4,
-        dims=1
-    ))
+#     vertices_44_3443_4 = unique(cat(
+#         vertices_44_34_4,
+#         vertices_44_43_4,
+#         dims=1
+#     ))
 
-    game1 = [
-        1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1;
-        0 1 0 0 1 0 0 0 0 0 0 1 0 0 1 0;
-        0 0 0 1 0 0 1 0 0 1 0 0 1 0 0 0;
-        0 0 1 0 0 0 0 1 1 0 0 0 0 1 0 0;
-    ]
-    game2 = [
-        1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1;
-        0 1 0 0 1 0 1 0 0 1 0 1 0 0 1 0;
-        0 0 1 0 0 0 0 1 1 0 0 0 0 1 0 0;
-        0 0 0 1 0 0 0 0 0 0 0 0 1 0 0 0;
-    ]
-    game1_scores = map(v -> (v, sum(game1[:].*v[:])), vertices_44_3443_4)
-    game2_scores = map(v -> (v, sum(game2[:].*v[:])), vertices_44_3443_4)
+#     game1 = [
+#         1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1;
+#         0 1 0 0 1 0 0 0 0 0 0 1 0 0 1 0;
+#         0 0 0 1 0 0 1 0 0 1 0 0 1 0 0 0;
+#         0 0 1 0 0 0 0 1 1 0 0 0 0 1 0 0;
+#     ]
+#     game2 = [
+#         1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1;
+#         0 1 0 0 1 0 1 0 0 1 0 1 0 0 1 0;
+#         0 0 1 0 0 0 0 1 1 0 0 0 0 1 0 0;
+#         0 0 0 1 0 0 0 0 0 0 0 0 1 0 0 0;
+#     ]
+#     game1_scores = map(v -> (v, sum(game1[:].*v[:])), vertices_44_3443_4)
+#     game2_scores = map(v -> (v, sum(game2[:].*v[:])), vertices_44_3443_4)
 
-    @test 8 == max(map(tuple -> tuple[2], game1_scores)...)
-    @test 10 == max(map(tuple -> tuple[2], game2_scores)...)
-end
-
-@testset "(6,3)->(2,3)->2 qmac  " begin
-    
-    vertices = multi_access_vertices(6,3,2,2,3)
-
-    diff_test = [
-        1 0 0 0 1 1 0 1 0 1 0 1 0 0 1 1 1 0;
-        0 1 1 1 0 0 1 0 1 0 1 0 1 1 0 0 0 1;
-    ]
-
-    raw_game = optimize_linear_witness(vertices, diff_test[1,:][:])
-    bell_game = convert(BellGame, round.(Int, 2*raw_game), BlackBox(2,18), rep="normalized")
-
-    @test raw_game ≈ [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
-end
-
-@testset "(8,3)->(2,3)->2 qmac  " begin
-    
-    vertices = multi_access_vertices(8,3,2,2,3)
-    gen_vertices = multi_access_vertices(8,3,2,2,3, normalize=false)
-
-    diff_test = [
-        1 1 1  1 1 0  1 0 1  1 0 0  0 1 1  0 1 0  0 0 1  0 0 0;
-        0 0 0  0 0 1  0 1 0  0 1 1  1 0 0  1 0 1  1 1 0  1 1 1;
-    ]
-
-    max_score = 0
-    for v in gen_vertices
-        score = sum(diff_test[:].*v[:])
-        if score > max_score
-            max_score = score
-        end
-    end
-    max_score
-
-    raw_game = optimize_linear_witness(vertices, diff_test[1,:][:])
-    bell_game = convert(BellGame, round.(Int, 2*raw_game), BlackBox(2,24), rep="normalized")
-    bell_game.β
-
-
-    println(raw_game)
-
-    @test raw_game ≈ [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
-end
+#     @test 8 == max(map(tuple -> tuple[2], game1_scores)...)
+#     @test 10 == max(map(tuple -> tuple[2], game2_scores)...)
+# end
